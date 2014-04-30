3b48105f1ee25efe9f30e0cc9dd008a2ea77fd93
Morris Jobke <morris.jobke@gmail.com>
2014-04-30 02:20:09 +0200

    first release

--- js/geojson.js
+++ js/geojson.js
@@ -1,4 +1,4 @@
-{
+var fireplaces = {
   "type": "FeatureCollection",
   "features": [
     {
@@ -11,6 +11,7 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "Richard-Hartmann-Platz",
         "Zeit": "20 bis 24 Uhr"
       }
@@ -25,6 +26,7 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "Sportplatz Guerickestraße 48 (Windweg)",
         "Zeit": "20 bis 24 Uhr"
       }
@@ -39,6 +41,7 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "An der Walzenmühle/Ecke Eigenhufe",
         "Zeit": "20 bis 24 Uhr"
       }
@@ -53,6 +56,7 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "Schulberg 5 in Glösa",
         "Zeit": "18 bis 23 Uhr"
       }
@@ -67,6 +71,7 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "Aussichtspunkt Pappel im Ortsteil Einsiedel",
         "Zeit": "20:30 bis 24 Uhr"
       }
@@ -81,6 +86,7 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "Kirchweg im Ortsteil Euba",
         "Zeit": "21 bis 24 Uhr"
       }
@@ -95,6 +101,7 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "Goetheweg 58-60/Ecke Rabensteiner Straße im Ortsteil Röhrsdorf",
         "Zeit": "19:30 bis 24 Uhr"
       }
@@ -109,9 +116,10 @@
         ]
       },
       "properties": {
+        "marker-symbol": "fire-station",
         "Ort": "Kreisigstraße gegenüber der Eselsbrücke in Rabenstein",
         "Zeit": "18 bis 24 Uhr"
       }
     }
 ]
-}
+};
